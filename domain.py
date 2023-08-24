8261b1e4682296139cee286af410b9a279da99cd
t-swini <tejaswini.kalluri2@gmail.com>
2023-08-24 11:47:15 -0700

    weblogic update

--- domain.py
+++ domain.py
@@ -1,14 +1,14 @@
-#!/u01/app/oracle/may23/new_middlware/wls/oracle_common/common/bin/wlst.sh
+#!/u01/app/oracle/may23/new_middleware/wls/oracle_common/common/bin/wlst.sh
 import os, sys
-readTemplate('/u01/app/oracle/may23/new_middlware/wls/wlserver/common/templates/wls/wls.jar')
+readTemplate('/u01/app/oracle/may23/new_middleware/wls/wlserver/common/templates/wls/wls.jar')
 cd('/Security/base_domain/User/weblogic')
 cmo.setPassword('weblogic1')
 cd('/Server/AdminServer')
 setOption('ServerStartMode', 'prod')
-cmo.setName('admin_test')
+cmo.setName('admin_teja')
 cmo.setListenPort(7001)
-cmo.setListenAddress('192.168.0.152')
-writeDomain('/u01/app/oracle/may23/new_middlware/wls/user_projects/domains/jenkins_test')
+cmo.setListenAddress('192.168.1.6')
+writeDomain('/u01/app/oracle/may23/new_middleware/wls/user_projects/domains/jenkins_teja')
 closeTemplate()
 print '>>>Domain created successfully>>>'
 exit()
